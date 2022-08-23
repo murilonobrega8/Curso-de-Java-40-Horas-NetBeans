ed54cb7fc98e1d1128f34493cc8a0182e04d315b
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-23 13:21:50 -0300

    update22-08-23

--- PrimeiroProgramaEmJava/src/main/java/com/mycompany/primeiroprogramaemjava/PrimeiroProgramaEmJava.java
+++ PrimeiroProgramaEmJava/src/main/java/com/mycompany/primeiroprogramaemjava/PrimeiroProgramaEmJava.java
@@ -3,6 +3,8 @@ public class PrimeiroProgramaEmJava {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
         System.out.println("Atualizando!");
-        System.out.println("Teste 02");
+        System.out.println("Teste 01...");
+        System.out.println("Teste 02...");
+        System.out.println("Teste 03, ok!");
     }
 }
