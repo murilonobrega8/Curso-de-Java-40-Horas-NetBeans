846a1de074b70123c68d319de5b51aa0bf2a8fb1
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-28 01:01:03 -0300

    created_TiposPrimitivos

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiposprimitivos;
+
+/**
+ *
+ * @author murilonobrega8
+ */
+public class TiposPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nome = "Murilo";
+        float nota = 8.5f;
+        System.out.println("A nota é " + nota);
+        System.out.printf("A nota é %.2f \n", nota);
+        System.out.printf("A nota de %s é %.4f \n", nome, nota);
+        System.out.format("A nota %.3f é de %s \n", nota, nome);
+    }
+    
+}
