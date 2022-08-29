e6189cfc857b080df154c7d07c3d8b180ad831b4
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-29 20:07:49 -0300

    Update TiposPrimitivos.java

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -24,6 +24,20 @@ public class TiposPrimitivos {
         System.out.printf("A nota de %s é %.4f \n", nome, nota);
         System.out.format("A nota %.3f é de %s \n", nota, nome);
         Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome2 = teclado.nextLine();
+        System.out.print("Digite a nota do aluno: ");
+        float nota2 = teclado.nextFloat();
+        System.out.format("A nota de %s é %.2f \n", nome2, nota2);
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+        String valor2 = "40";
+        int idade2 = Integer.parseInt(valor2);
+        System.out.println(idade2);
+        String valor3 = "30.75";
+        float idade3 = Float.parseFloat(valor3);
+        System.out.println(idade3);
     }
     
 }
