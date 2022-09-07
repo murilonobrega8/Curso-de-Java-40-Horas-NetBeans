a8ef7aff485a7bbede42967a0a09f57d8813f4e1
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-07 02:15:28 -0300

    created_Repeticao2

---
+++ ContadorCambalhotaFinal/src/contadorcambalhotafinal/ContadorCambalhotaFinal.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package contadorcambalhotafinal;
+
+/**
+ *
+ * @author murilonobrega8
+ */
+public class ContadorCambalhotaFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contcamb = 1;
+        do {
+            System.out.println("Cambalhota nº " + contcamb);
+            contcamb++;
+        } while (contcamb <= 5);
+        
+    }
+    
+}
