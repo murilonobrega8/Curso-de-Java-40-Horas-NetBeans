6f32b59743af92840927342430ec88891ec6a192
Murilo Oliveira <murilonobrega8@gmail.com>
2022-09-07 17:27:23 -0300

    created_RepeticaoComFor

---
+++ RepeticaoComFor/src/repeticaocomfor/RepeticaoComFor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaocomfor;
+
+/**
+ *
+ * @author murilonobrega8
+ */
+public class RepeticaoComFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int contador=0; contador <=3; contador++) {
+            System.out.println("Cambalhota nº " + (contador+1));
+        }
+        
+    }
+    
+}
